dda57e9ccdddd2bbeea95b3f1b6a4ee289c9b55d
Charlie Birks <charlie@daftgames.net>
2020-02-23 15:39:05 +0000

    override sbrk so that allocation can fail

--- src/blit/main.cpp
+++ src/blit/main.cpp
@@ -6,6 +6,28 @@
 extern void D_DoomMain();
 extern void D_Display();
 
+#ifdef TARGET_32BLIT_HW
+extern "C" void *_sbrk(ptrdiff_t incr)
+{
+    extern char end, __ltdc_start;
+    static char *heap_end;
+
+    if(!heap_end)
+        heap_end = &end;
+
+    // ltdc is at the end of the heap
+    if(heap_end + incr > &__ltdc_start)
+    {
+        puts("sbrk oom");
+        return (void *)-1;
+    }
+
+    char *ret = heap_end;
+    heap_end += incr;
+
+    return (void *)ret;
+}
+#endif
 
 void init()
 {
