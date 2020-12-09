072498eaeec0f3ffc93532187938eb45288402bf
Charlie Birks <charlie@daft.games>
2020-12-09 16:56:35 +0000

    Fix append_wads to handle the RELO header

--- append_wads.py
+++ append_wads.py
@@ -12,15 +12,22 @@ args = parser.parse_args()
 # grab metadata / length
 in_file = open(args.input_file, "rb")
 head = in_file.read(20)
+head_off = 0
+
+if head[:4] == b'RELO':
+    num_relocs = struct.unpack('<I', head[4:8])[0]
+    head_off = num_relocs * 4 + 8
+    in_file.seek(head_off)
+    head = in_file.read(20)
 
 in_file_end = struct.unpack('<I', head[16:20])[0] & 0x1FFFFFF
 
-in_file.seek(in_file_end)
+in_file.seek(in_file_end + head_off)
 metadata = in_file.read()
 in_file.seek(0)
 
 out_file = open(args.output_file, "wb")
-out_file.write(in_file.read(in_file_end))
+out_file.write(in_file.read(in_file_end + head_off))
 
 # header
 out_file.write(b"APPFILES")
@@ -35,9 +42,9 @@ for filename in args.append_file:
     out_file.write(basename.encode())
     out_file.write(open(filename, "rb").read())
 
-new_end = out_file.tell()
+new_end = out_file.tell() - head_off
 out_file.write(metadata)
 
 # patch end pointer
-out_file.seek(16)
+out_file.seek(head_off + 16)
 out_file.write(struct.pack("<I", 0x90000000 | new_end))
